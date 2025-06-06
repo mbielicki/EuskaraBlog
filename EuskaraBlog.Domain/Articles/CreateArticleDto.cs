4193371a19faba84bc700d0901ded812d5d0c803
Michał Bielicki <mbielicki2912@gmail.com>
2025-06-06 19:47:12 +0200

    add validation to CreateArticleDto

--- EuskaraBlog.Domain/Articles/CreateArticleDto.cs
+++ EuskaraBlog.Domain/Articles/CreateArticleDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,11 @@ namespace EuskaraBlog.Domain.Articles
 {
     public class CreateArticleDto
     {
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
         public string Title { get; set; } = string.Empty;
+
+        [Required]
         public string Content { get; set; } = string.Empty;
     }
 }
