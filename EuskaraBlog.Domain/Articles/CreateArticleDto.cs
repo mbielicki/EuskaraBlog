74e112850a371649ade9383274dbf1a82bb89ab7
Michał Bielicki <mbielicki2912@gmail.com>
2025-06-06 19:37:56 +0200

    add CreateArticleDto

---
+++ EuskaraBlog.Domain/Articles/CreateArticleDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuskaraBlog.Domain.Articles
+{
+    public class CreateArticleDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+    }
+}
